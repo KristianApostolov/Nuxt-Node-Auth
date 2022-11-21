a6bd975b222ea65dd2b9f159dbb329a375278ef6
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2022-11-21 23:20:43 +0200

    change from static string to dynamic variable

--- frontend/utility/getUser.ts
+++ frontend/utility/getUser.ts
@@ -1,14 +1,16 @@
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse } from "axios";
+import { apiUrl } from "~~/config";
 
-const getUser = async (sessionId:  string) => {
-    const user: object = await axios.post("http://localhost:4000/user/get", {
-        "session_id": sessionId
+const getUser = async (sessionId: string) => {
+  const user: object = await axios
+    .post(`${apiUrl}/user/get`, {
+      session_id: sessionId,
     })
     .then((response: AxiosResponse) => {
-        return response.data.user
-    })
+      return response.data.user;
+    });
 
-    return user
-}
+  return user;
+};
 
-export default getUser
+export default getUser;
